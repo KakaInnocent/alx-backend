192fc2b8979601b54b68f9a1175e9f35bd676e6e
KakaInnocent <saneugene847@gmail.com>
2023-03-01 10:29:54 +0000

    last mandatory task

--- 0x02-i18n/7-app.py
+++ 0x02-i18n/7-app.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python3
 
 """
-use user locale
+7. Basic Flask app
 """
+
 
 from flask import Flask, render_template, request, g
 from flask_babel import Babel
+import pytz
+from pytz.exceptions import UnknownTimeZoneError
 
 app = Flask(__name__)
 babel = Babel(app)
@@ -13,7 +16,7 @@ babel = Babel(app)
 
 class Config:
     """
-    make configurations
+    Config class.
     """
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
@@ -32,7 +35,7 @@ users = {
 
 def get_user(login_as):
     """
-    get user
+    get_user.
     """
     try:
         return users.get(int(login_as))
@@ -43,7 +46,7 @@ def get_user(login_as):
 @app.before_request
 def before_request():
     """
-    before request
+    before_request
     """
     g.user = get_user(request.args.get("login_as"))
 
@@ -51,7 +54,7 @@ def before_request():
 @babel.localeselector
 def get_locale():
     """
-    get locale.
+    get_locale.
     """
     locale = request.args.get("locale")
     if locale:
@@ -67,12 +70,34 @@ def get_locale():
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
+@babel.timezoneselector
+def get_timezone():
+    """
+    get_timezone.
+    """
+    try:
+        timezone = request.args.get("timezone")
+        if timezone:
+            return pytz.timezone(timezone)
+        user = request.args.get("login_as")
+        if user:
+            timezone = users.get(int(user)).get('timezone')
+            if timezone:
+                return pytz.timezone(timezone)
+        timezone = request.headers.get("timezone")
+        if timezone:
+            return pytz.timezone(timezone)
+    except UnknownTimeZoneError:
+        return app.config.get('BABEL_DEFAULT_TIMEZONE')
+    return app.config.get('BABEL_DEFAULT_TIMEZONE')
+
+
 @app.route('/', methods=["GET"], strict_slashes=False)
 def hello():
     """
-    return string
+    hello.
     """
-    return render_template('6-index.html')
+    return render_template('7-index.html')
 
 
 if __name__ == "__main__":
